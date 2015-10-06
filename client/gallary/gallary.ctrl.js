edda88173a6bef9508fdc9d0ff257f41599a1ded
RedWood <mackey91@gmail.com>
2015-10-06 12:10:40 -0700

    imgur replacing instagram

--- client/gallary/gallary.ctrl.js
+++ client/gallary/gallary.ctrl.js
@@ -1,11 +1,22 @@
 angular.module('app')
   .controller('gallaryCtrl', ['$scope', '$http', '$interval', '$timeout', '$anchorScroll',
     function ($scope, $http, $interval, $timeout, $anchorScroll) {
+
+      // loop through photo collection
+      // randomly select a photo to be flipped
+      // set flipped to true
+      // if flipped is true don't flip
+      // 
+
+      
+      
       $scope.scrollTo = function(id){
         $location.hash(id);
         $anchorScroll();
       };
+
       $scope.instagram = [];
+
       $scope.imgur = [];
       $scope.loading = false;
       $scope.counter = 10;
@@ -17,32 +28,57 @@ angular.module('app')
 
       $timeout(countDown, 1000);
 
-      $interval(function() {
+      // $interval(function() {
         $scope.counter = 11;
-        var mytimeout = $timeout($scope.onTimeout,1000);
-
-        $http.get('/imgur').success(function (data) {
-          for (var j = 0; j < 3; j+=1) {
-            $scope.imgur.push({url:data.data[j].link});
-          }
-          console.log($scope.imgur);
-        });
+        var mytimeout = $timeout($scope.onTimeout, 1000);
 
         while ($scope.instagram.length !== 0 && $scope.imgur !== 0) {
           $scope.instagram.pop();
           $scope.imgur.pop();
         }
 
-        $http.get('/instagram').success(function (data) {
-          
-          for (var i = 0; i < 6; i+=1) {
-            $scope.instagram.push(data.data[i].user);
-            $scope.instagram.push(data.data[i].images.low_resolution);
+        $http.get('/imgur').success(function (data) {
+          for (var j = 0; j < 5; j+=1) {
+            $scope.imgur.push({url: data.data[j].link, flipped: false});
           }
-          
-          $scope.loading = true;
+          console.log($scope.imgur);
         });
 
-    }, 10000);
+        $http.get('/instagram').success(function (data) {
+          for (var i = 0; i < 10; i+=1) {
+            var randomNum = Math.floor(Math.random()* 10 + 1);
+            $scope.instagram.push({username: data.data[i].user.username, flipped: false, url: data.data[i].images.low_resolution.url});
+              // selects random photo
+              if (i === randomNum) {
+                console.log(i, 'i am i');
+                (function (index) {
+                  $timeout(function() {
+                    console.log(index, ' i am index');
+                    $scope.instagram[index].flipped = true;
+                    $scope.instagram[index + 4].flipped = true;
+                    $scope.instagram[index - 2].flipped = true;  
+                  }, 2000);
+                })(i);
+               
+              }
+              
+          }
+          console.log($scope.instagram);
+        });
+
+
+        $scope.loading = true;
+    // }, 6000);
+        
+        // while ($scope.instagram.length !==0) {
+        //   console.log('hello');
+        //   for (var i = 0; i < $scope.instagram.length; i+=1) {
+            
+        //     var randomInstaPhoto = $scope.instagram[randomNum];
+        //     console.log(randomInstaPhoto);
+        //   }
+        // }
+
+        
 
 }]);
--- client/home/home.ctrl.js
+++
@@ -1,4 +0,0 @@
-angular.module('app')
-  .controller('homeCtrl', ['$scope', function($scope) {
-    $scope.name = 'allen';
-  }]);
