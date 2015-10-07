ef846aa113dcdb8665d953619ee379dbec174542
RedWood <mackey91@gmail.com>
2015-10-07 13:10:33 -0700

    deploy

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('client'));
+app.use(express.static(__dirname + '/client'));
 app.use(require('./photoApiRoutes'));
 
+app.set('port', (process.env.PORT || 5000));
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
 
