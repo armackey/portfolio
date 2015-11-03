ed3d1d0e8a688530e5c0a4c868e739a873f2e916
RedWood <mackey91@gmail.com>
2015-11-03 01:52:47 -0800

    added link to random meet

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.use(express.static('client'));
 app.use(require('./photoApiRoutes'));
-
+// app.use(express.static(path.join(path.normalize(__dirname + 'client'))));
 app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
